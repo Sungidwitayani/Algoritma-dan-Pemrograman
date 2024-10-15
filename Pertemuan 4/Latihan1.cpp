a916339aa17c3825c7c083f94ef3240ac6d6f53c
Sungidwitayani <sungidwitayani@gmail.com>
2024-10-15 13:01:58 +0800

    Add files via upload

---
+++ Pertemuan 4/Latihan1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+
+    if (a > b) { // menambahkan kurung kurawa
+        cout << "a is greater than b" << endl;
+    } else { // Menambahkan kurung kurawa
+        cout << "b is greater than a" << endl; // Menambahkan titik koma
+    } // Menambahkan kurung kurawa
+    return 0;
+}
---
+++ Pertemuan 4/Latihan4.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+void printMessage(){
+    cout << "Hello, world!" << endl;
+}
+int main() {
+    printMessage(); // Menambahkan kurung buka dan kurung tutup
+    return 0;
+}
